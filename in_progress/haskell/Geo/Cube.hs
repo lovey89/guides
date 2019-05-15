43f9c61a41b24d2fbdacf9063ab97d1460da1bed
Love Yregård <xlove.github@yregard.se>
2019-05-15 22:45:47 +0200

    Haskell guide

---
+++ in_progress/haskell/Geo/Cube.hs
@@ -0,0 +1,12 @@
+module Geo.Cube
+( volume
+, area
+) where
+
+import qualified Geo.Cuboid as Cuboid
+
+volume :: Float -> Float
+volume side = Cuboid.volume side side side
+
+area :: Float -> Float
+area side = Cuboid.area side side side

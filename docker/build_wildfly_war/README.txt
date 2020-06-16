69bcfab51caeae053eea3e754be21452b6abc504
Love Yregård <xlove.github@yregard.se>
2020-06-16 23:13:39 +0200

    Docker

---
+++ docker/build_wildfly_war/README.txt
@@ -0,0 +1,1 @@
+All code in this directory is found at https://github.com/mechero/spring-boot-jboss-wildfly
---
+++ docker/build_wildfly_war/src/main/java/com/thepracticaldeveloper/jbosswildflydemo/HolaController.java
@@ -0,0 +1,19 @@
+package com.thepracticaldeveloper.jbosswildflydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author moises.macero
+ */
+@RestController
+@RequestMapping("/hola")
+public final class HolaController {
+
+    @GetMapping
+    public String dimeHola(){
+        var message = "Hola Amigo!";
+        return message;
+    }
+}

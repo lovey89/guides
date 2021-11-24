a3f6171e28502a5f74348dac5d0da4690ee86f4c
Love Yregård <xlove.github@yregard.se>
2021-11-24 23:33:22 +0100

    Rust

--- in_progress/rust/002_guessing_game/guessing_game/src/main.rs
+++ in_progress/rust/002_guessing_game/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 // The io library comes from the standard library (std)
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,23 +8,49 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // The 'mut' keyword makes the variable mutable
-    // In Rust, variables are immutable by default
-    let mut guess = String::new();
+        // The 'mut' keyword makes the variable mutable
+        // In Rust, variables are immutable by default
+        let mut guess = String::new();
 
-    // If the 'use' import on the second line wasn't present we could have written
-    // std::io::stdin
-    io::stdin()
-        // Append (don't overwrite) the guess string
-        // & indicates that this is a reference
-        // references are also immutable, so we need to use mut once again
-        .read_line(&mut guess)
-        // A Result enum is returned indicating either 'Ok' or 'Err'
-        .expect("Failed to read line");
+        // If the 'use' import on the second line wasn't present we could have written
+        // std::io::stdin
+        io::stdin()
+            // Append (don't overwrite) the guess string
+            // & indicates that this is a reference
+            // references are also immutable, so we need to use mut once again
+            .read_line(&mut guess)
+            // A Result enum is returned indicating either 'Ok' or 'Err'
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        // This guess variable will shadow the previous shadow
+        // The parse method on strings parses a string into some kind of number
+        // but rust will infer the type to u32 since we have declared the variable
+        // that way
+        // The following code will crash the program if you enter a non-number
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        /*
+          The cmp method compares two values and can be called on anything that can
+          be compared. It takes a reference to whatever you want to compare with
+        */
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }

26f93da61ad55dd88a2af16f469e8a4e752a1a97
Love Yregård <xlove.github@yregard.se>
2022-12-05 00:00:48 +0100

    Rust

--- in_progress/rust/002_guessing_game/guessing_game/src/main.rs
+++ in_progress/rust/002_guessing_game/guessing_game/src/main.rs
@@ -1,12 +1,16 @@
 // The io library comes from the standard library (std)
-use std::io;
 use std::cmp::Ordering;
+use std::io;
+// The rand crate is a dependency. It's mentioned in the Cargo.toml file
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // Old code looked like
+    //let secret_number = rand::thread_rng().gen_range(1..101);
+    // So I guess that "=x"" means that x is inclusive and just "x" is exclusive
 
     //println!("The secret number is: {}", secret_number);
 
@@ -25,9 +29,10 @@ fn main() {
             // references are also immutable, so we need to use mut once again
             .read_line(&mut guess)
             // A Result enum is returned indicating either 'Ok' or 'Err'
+            // If the values is err, this program will panic with the provided message
             .expect("Failed to read line");
 
-        // This guess variable will shadow the previous shadow
+        // This guess variable will shadow the previous guess variable
         // The parse method on strings parses a string into some kind of number
         // but rust will infer the type to u32 since we have declared the variable
         // that way
@@ -39,10 +44,14 @@ fn main() {
         };
 
         println!("You guessed: {}", guess);
+        // The alternative form below is also ok
+        //println!("You guessed: {guess}");
 
         /*
           The cmp method compares two values and can be called on anything that can
-          be compared. It takes a reference to whatever you want to compare with
+          be compared. It takes a reference to whatever you want to compare with.
+          Here rust will infer that secret_number must be of type u32 since guess
+          is of type u32
         */
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),

783f2fdff4326363b29d4ef9e0fad061d372fc2a
Love Yregård <xlove.github@yregard.se>
2024-08-03 11:52:10 +0200

    Rust

--- in_progress/rust/007_minigrep/minigrep/src/lib.rs
+++ in_progress/rust/007_minigrep/minigrep/src/lib.rs
@@ -22,7 +22,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- in_progress/rust/007_minigrep/minigrep/src/main.rs
+++ in_progress/rust/007_minigrep/minigrep/src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);

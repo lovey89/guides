29739fb765fcf355e3930ad13eb048cd1574f5d7
Love Yregård <xlove.github@yregard.se>
2025-07-16 18:34:31 +0200

    Vue

--- in_progress/vue/vue-sandbox/src/components/05_routing/AboutView.vue
+++ in_progress/vue/vue-sandbox/src/components/05_routing/AboutView.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+
+import { NButton, NModal, NCard } from 'naive-ui'
 
 const router = useRouter()
 const route = useRoute()
@@ -13,6 +15,8 @@ const search = computed({
     router.replace({ query: { search } })
   }
 })
+
+const showModal = ref(false);
 </script>
 
 <template>
@@ -20,4 +24,34 @@ const search = computed({
   <label>
     Search: <input v-model.trim="search" maxlength="20">
   </label>
+  <NButton @click="showModal = true">
+    Start me up
+  </NButton>
+  <NModal v-model:show="showModal">
+    <NCard
+
+      title="Modal"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="false"
+      closable="true"
+    >
+      <template #header-extra>
+        Oops!
+      </template>
+      <div style="width: 1000px;">
+        Content
+      </div>
+      <template #footer>
+        Footer
+      </template>
+    </NCard>
+  </NModal>
 </template>
+
+<style scoped>
+  .n-card {
+    width: 70%;
+  }
+</style>
